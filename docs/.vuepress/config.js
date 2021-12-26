5543ea60173937d37546ee5437d688cc11fe12a9
ruohuan <2055761251@qq.com>
2021-12-26 20:53:38 +0800

    feat: pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,23 +3,40 @@ moment.locale("zh-cn");
 
 
 module.exports = {
-    base: "/docs/",
+   
     title: "罪亦为原罪",
     description: "知行合一",
     head: [
         ['link', { rel: 'icon', href: '/ac.svg' }],
         ['meta', { name: 'author', content: '罪亦为原罪' }],
-        ['meta', { name: 'keywords', content: 'vuepress 介绍, vuepress 说明,罪亦为原罪' }]
+        ['meta', { name: 'keywords', content: 'vuepress 介绍, vuepress 说明,罪亦为原罪' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
     ],
 
     plugins: [
         [
-          '@vuepress/last-updated',
-          {
+          '@vuepress/last-updated', {
             transformer: (timestamp) =>{
               return moment(timestamp).format("LLLL")
             }
           }
+        ],
+        [
+          '@vuepress/pwa', {
+                    serviceWorker: true,
+                    updatePopup: {
+                    message: "发现新内容可用",
+                    buttonText: "刷新"
+                  }
+                }
         ]
     ],
     themeConfig: {
