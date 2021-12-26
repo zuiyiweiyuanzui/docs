a78c78d9625211cecad2dee64ead0951bec31b74
ruohuan <2055761251@qq.com>
2021-12-26 20:56:02 +0800

    feat: pwa2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,6 @@ module.exports = {
         ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
         ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
         ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
-  ],
     ],
 
     plugins: [
